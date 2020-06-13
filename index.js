600b72c29b24442f31c11b28c5dd3d4464477eee
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-06-13 21:28:27 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+// This file would be created soon
+
+require('dotenv').config();
+
+const token = process.env.BOT_TOKEN;
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+    bot = new TelegramBot(token);
+    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+    bot = new TelegramBot(token, {polling: true});
+}
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
