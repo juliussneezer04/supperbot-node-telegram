76ca39dd0705690de9a0963c008188a5d6e21797
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-07-01 12:26:40 +0800

    update new menu format

--- db/extractMenu.js
+++ db/extractMenu.js
@@ -3,14 +3,14 @@ const YAML = require('yaml')
 const fs = require('fs');
 
 async function run() {
-    const root = {
+    let root = {
         "name": "Al_Amaans",
         "id": 0
     }
-    root["children"] = await queries.getChildren({
-        "menu": 0,
-        "item_id": root["id"]
-    });
+    // root["children"] = await queries.getChildren({
+    //     "menu": 0,
+    //     "item_id": root["id"]
+    // });
 
     async function populateChildren(item) {
         if (!await queries.isTerminal({
@@ -26,19 +26,39 @@ async function run() {
                 children[i] = await populateChildren(item["children"][i]);
             }
         }
-        delete item["id"];
+        // get modifiers
+        // let level = 0;
+        // let modifiers = [];
+        // let mod = true;
+        // while(mod){
+        //      let levelMod = await queries.getModifierOptions({
+        //         "menu": 0,
+        //         "item_id": item["id"],
+        //         "level": level,
+        //     });
+        //      if(levelMod.length === 0){
+        //          mod = false;
+        //      } else{
+        //          level++;
+        //          modifiers.concat(levelMod);
+        //      }
+        // }
         if (item.hasOwnProperty("children")) {
-            item[item["name"]] = item["children"];
-            delete item["children"];
-            delete item["name"];
+            let obj = {};
+            obj[item["name"]] = {};
+            for (let i = 0; i < item["children"].length; i++) {
+                Object.assign(obj[item["name"]], item["children"][i]);
+            }
+            return obj;
         } else {
-            return item["name"];
+            let obj = {};
+            obj[item["name"]] = {"price": item["price"]};
+            return obj;
         }
-        return item;
     }
-    await populateChildren(root);
+    root = await populateChildren(root);
 
-    const y = YAML.stringify(root);
+    const y = await YAML.stringify(root);
     fs.writeFile("menu.yml", y, () => {});
     return;
 }
