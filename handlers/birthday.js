e6cc9f9e342992539c338c1de01507ad62796484
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-07-24 18:18:05 +0800

    print log for happy birthday
    not working on heroku

--- handlers/birthday.js
+++ handlers/birthday.js
@@ -3,6 +3,7 @@ const queries = require('../db/queries');
 
 module.exports.birthday = async function (msg) {
     const originalText = msg.text;
+    console.log("entered birthday function with text " + originalText + " from " + msg.from.username);
     const lowerText = originalText.toLowerCase();
     const stringsToMatch = ["happybirthday", "happybday"];
     let matched = false;
@@ -12,6 +13,7 @@ module.exports.birthday = async function (msg) {
             break;
         }
     }
+    console.log("matched: " + matched);
     if (matched) {
         const count = await queries.repeatCount(originalText);
         if (count === 9) {
