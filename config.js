e7cd4086d5a635f637688aed5844fdbf8ff6fbdd
nicktohzyu <nicholastohzhenyu@gmail.com>
2021-03-11 15:27:03 +0800

    Update config.js
    fix "no pg_hba.conf entry for host" error

--- config.js
+++ config.js
@@ -20,7 +20,8 @@ module.exports.db_config = {
     host: process.env.DB_HOST, // Server hosting the postgres database
     port: 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed,
+    ssl: { rejectUnauthorized: false }
 };
 
 module.exports.menus = [
