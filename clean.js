7b15b6bb564372197a447ce52405f004534e58c7
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2021-12-13 15:58:23 +0800

    Revert "Update scheduled cleaning"

--- clean.js
+++
@@ -1,5 +0,0 @@
-const queries = require("./db/queries");
-
-queries.clearOldEntries('jiodata', 'jios');
-queries.clearOldEntries('miscellaneous', 'helper');
-queries.clearOldEntries('miscellaneous', 'cache');
